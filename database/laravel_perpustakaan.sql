a5b684c8b2117123cfbf75344b9a84449e9e7343
irvanurhakim93 <irvan.nurhakim1993@gmail.com>
2023-04-18 11:20:38 +0700

    updated 14/04/2023

--- database/laravel_perpustakaan.sql
+++ database/laravel_perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 10 Apr 2023 pada 14.09
+-- Waktu pembuatan: 18 Apr 2023 pada 06.17
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.0.25
 
@@ -43,10 +43,11 @@ CREATE TABLE `anggota` (
 --
 
 INSERT INTO `anggota` (`id`, `nama`, `alamat`, `telepon`, `email`, `status_aktif`, `created_at`, `updated_at`) VALUES
-(8, 'anggota1', 'ala anggota1', '1242142353254', 'anggota1@gmail.com', 1, '2023-04-09 10:17:22', '0000-00-00 00:00:00'),
-(10, 'test1', 'Jaksel', '2321312321432432', 'anggota3@gmail.com', 1, '2023-04-09 10:19:42', '2023-04-09 03:19:42'),
-(11, 'Sari', 'Jakarta', '0821', 'saricantik@gmail.com', 1, '2023-04-09 04:46:52', '2023-04-09 04:46:52'),
-(12, 'Nergal Darski', 'Norwegia', '021666', 'behemoth@gmail.com', 1, '2023-04-10 01:35:25', '2023-04-10 01:35:25');
+(1, 'anggota1', 'ala anggota1', '1242142353254', 'anggota1@gmail.com', 1, '2023-04-09 10:17:22', '0000-00-00 00:00:00'),
+(2, 'test1', 'Jaksel', '2321312321432432', 'anggota3@gmail.com', 1, '2023-04-09 10:19:42', '2023-04-09 03:19:42'),
+(3, 'Sari', 'Jakarta', '0821', 'saricantik@gmail.com', 1, '2023-04-09 04:46:52', '2023-04-09 04:46:52'),
+(4, 'Nergal Darski', 'Norwegia', '021666', 'behemoth@gmail.com', 1, '2023-04-10 01:35:25', '2023-04-10 01:35:25'),
+(5, 'Rere Reza', 'Bandung', '022', 'reregrass@gmail.com', 1, '2023-04-15 19:17:06', '2023-04-15 19:17:06');
 
 -- --------------------------------------------------------
 
@@ -72,11 +73,13 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id`, `nama`, `tahun_terbit`, `id_penulis`, `id_penerbit`, `id_kategori`, `sinopsis`, `sampul`, `created_at`, `updated_at`) VALUES
-(6, 'Apa aja', 2018, 1, 1, 1, 'zsdsa', 'Apa aja_Koala.jpg', '2023-04-09 06:15:54', '0000-00-00 00:00:00'),
-(8, 'Si Kancil', 2000, 1, 1, 1, 'Kancil yang hobi mencuri', NULL, '2023-04-09 09:41:21', '2023-04-09 02:41:21'),
-(9, 'Buku Pemrograman HTML 5', 2018, 3, 3, 3, 'Buku tutorial pemrograman HTML 5', NULL, '2023-04-08 23:18:53', '2023-04-08 23:18:53'),
-(10, 'Buku Chord Gitar', 2001, 1, 1, 2, 'Tutorial chord lagu lagu indonesia paling terbaru dan terhits pokoknya', NULL, '2023-04-09 09:39:29', '2023-04-09 02:39:29'),
-(11, 'Komik Siksa Neraka', 1990, 1, 1, 1, 'Komik yang menceritakaan siksaan kepada manusia di neraka atas perbuatan dosa selama di dunia. Dijamin siapa yang membacanya akan auto tobat', '1681022998.jpg', '2023-04-09 09:40:33', '2023-04-09 02:40:33');
+(1, 'Apa aja', 2018, 2, 2, 2, 'Tentang bacaan bagi orang orang yang tidak memliki harapan hidup', 'Apa aja_Koala.jpg', '2023-04-16 19:18:24', '2023-04-16 12:18:24'),
+(2, 'Si Kancil', 2000, 3, 2, 5, 'Kancil yang hobi mencuri, kali ini pencuriannya lebih modern seperti mencuri motor sport', NULL, '2023-04-16 19:16:42', '2023-04-16 12:16:42'),
+(3, 'Buku Pemrograman HTML 5', 2018, 3, 1, 2, 'Buku pemrograman html terkini', NULL, '2023-04-17 06:15:32', '2023-04-16 23:15:32'),
+(4, 'Buku Chord Gitar', 2001, 1, 1, 2, 'Tutorial chord lagu lagu indonesia paling terbaru dan terhits pokoknya', NULL, '2023-04-09 09:39:29', '2023-04-09 02:39:29'),
+(5, 'Komik Siksa Neraka', 1990, 1, 1, 1, 'Komik yang menceritakaan siksaan kepada manusia di neraka atas perbuatan dosa selama di dunia. Dijamin siapa yang membacanya akan auto tobat', '1681022998.jpg', '2023-04-09 09:40:33', '2023-04-09 02:40:33'),
+(6, 'Buku Biografi Sulfikar Hartono', 2018, 3, 2, 1, 'Biografi tentang sulfikar hartono,si pembalap yang gugur pada saat acara balapan di malam takbiran', NULL, '2023-04-17 06:31:11', '2023-04-16 23:29:26'),
+(7, 'Kisah Inspiratif Jack Ma', 2020, 1, 2, 2, 'Biografi tentang pengusaha sukses Jack Ma yang mendirikan perusahaan Alibaba', NULL, '2023-04-16 23:33:28', '2023-04-16 23:33:28');
 
 -- --------------------------------------------------------
 
@@ -115,53 +118,6 @@ INSERT INTO `kategori` (`id`, `nama`) VALUES
 (3, 'Cerpen'),
 (4, 'Puisi'),
 (5, 'Novel');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data untuk tabel `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2023_04_08_071654_create_permission_tables', 1);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `model_has_permissions`
---
-
-CREATE TABLE `model_has_permissions` (
-  `permission_id` bigint(20) UNSIGNED NOT NULL,
-  `model_type` varchar(255) NOT NULL,
-  `model_id` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `model_has_roles`
---
-
-CREATE TABLE `model_has_roles` (
-  `role_id` bigint(20) UNSIGNED NOT NULL,
-  `model_type` varchar(255) NOT NULL,
-  `model_id` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -219,7 +175,7 @@ CREATE TABLE `penerbit` (
 
 INSERT INTO `penerbit` (`id`, `nama`, `alamat`, `telepon`, `email`) VALUES
 (1, 'Polindra', 'Jl. Kebenaran 104', '087733195026', 'polindra@gmail.com'),
-(2, 'PT. Syahrul', 'Jl. Kebenaran 303', '087733195025', 'pt.syahrul@gmail.com');
+(2, 'PT.Mencerdaskan Bangsa', 'Jl. Kebenaran 303', '087733195025', 'pt.syahrul@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -243,20 +199,6 @@ INSERT INTO `penulis` (`id`, `nama`, `alamat`, `telepon`, `email`) VALUES
 (1, 'Syahrul', 'Jl. Kebenaran 101', '087733195029', 'syahrul@gmail.com'),
 (2, 'Samsul', 'Jl. Kebenaran 102', '087733195028', 'samsul@gmail.com'),
 (3, 'Maul', 'Jl. Kebenaran 103', '087733195027', 'maul@gmail.com');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `permissions`
---
-
-CREATE TABLE `permissions` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `guard_name` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -295,8 +237,8 @@ CREATE TABLE `petugas` (
 --
 
 INSERT INTO `petugas` (`id`, `nama`, `alamat`, `telepon`, `email`) VALUES
-(4, 'petugas1', 'ala petugas1', '2312321321321', 'petugas1@gmail.com'),
-(5, 'petugas2', 'ala petugas2', '1232134243254', 'petugas2@gmail.com');
+(1, 'petugas1', 'ala petugas1', '2312321321321', 'petugas1@gmail.com'),
+(2, 'petugas2', 'ala petugas2', '1232134243254', 'petugas2@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -305,23 +247,18 @@ INSERT INTO `petugas` (`id`, `nama`, `alamat`, `telepon`, `email`) VALUES
 --
 
 CREATE TABLE `roles` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `guard_name` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Struktur dari tabel `role_has_permissions`
+-- Dumping data untuk tabel `roles`
 --
 
-CREATE TABLE `role_has_permissions` (
-  `permission_id` bigint(20) UNSIGNED NOT NULL,
-  `role_id` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `roles` (`id`, `nama`) VALUES
+(1, 'admin'),
+(2, 'anggota'),
+(3, 'petugas');
 
 -- --------------------------------------------------------
 
@@ -333,60 +270,20 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(25) NOT NULL,
-  `id_anggota` int(11) DEFAULT NULL,
-  `id_petugas` int(11) DEFAULT NULL,
-  `id_user_role` int(11) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL
+  `id_user_role` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `id_anggota`, `id_petugas`, `id_user_role`, `status`) VALUES
-(1, 'Admin', 'Admin', 0, 0, 1, 1),
-(14, 'anggota1', 'anggota1', 8, 0, 2, 2),
-(15, 'anggota2', 'anggota2', 9, 0, 2, 2),
-(16, 'petugas1', 'petugas1', 0, 4, 3, 3),
-(17, 'petugas2', 'petugas2', 0, 5, 3, 3),
-(18, 'test1', 'test12345', 10, 0, 2, 2);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  `remember_token` varchar(100) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user_role`
---
-
-CREATE TABLE `user_role` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data untuk tabel `user_role`
---
-
-INSERT INTO `user_role` (`id`, `nama`) VALUES
-(1, 'Admin'),
-(2, 'Anggota'),
-(3, 'Petugas');
+INSERT INTO `user` (`id`, `username`, `password`, `id_user_role`) VALUES
+(1, 'Admin', 'Admin', 1),
+(2, 'anggota1', 'anggota1', 2),
+(3, 'anggota2', 'anggota2', 2),
+(4, 'petugas1', 'petugas1', 3),
+(5, 'petugas2', 'petugas2', 3),
+(6, 'test1', 'test12345', 2);
 
 --
 -- Indexes for dumped tables
@@ -418,26 +315,6 @@ ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `model_has_permissions`
---
-ALTER TABLE `model_has_permissions`
-  ADD PRIMARY KEY (`permission_id`,`model_id`,`model_type`),
-  ADD KEY `model_has_permissions_model_id_model_type_index` (`model_id`,`model_type`);
-
---
--- Indeks untuk tabel `model_has_roles`
---
-ALTER TABLE `model_has_roles`
-  ADD PRIMARY KEY (`role_id`,`model_id`,`model_type`),
-  ADD KEY `model_has_roles_model_id_model_type_index` (`model_id`,`model_type`);
-
---
 -- Indeks untuk tabel `password_resets`
 --
 ALTER TABLE `password_resets`
@@ -462,13 +339,6 @@ ALTER TABLE `penulis`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `permissions`
---
-ALTER TABLE `permissions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `permissions_name_guard_name_unique` (`name`,`guard_name`);
-
---
 -- Indeks untuk tabel `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
@@ -486,33 +356,12 @@ ALTER TABLE `petugas`
 -- Indeks untuk tabel `roles`
 --
 ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `roles_name_guard_name_unique` (`name`,`guard_name`);
-
---
--- Indeks untuk tabel `role_has_permissions`
---
-ALTER TABLE `role_has_permissions`
-  ADD PRIMARY KEY (`permission_id`,`role_id`),
-  ADD KEY `role_has_permissions_role_id_foreign` (`role_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
--- Indeks untuk tabel `user_role`
---
-ALTER TABLE `user_role`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -523,13 +372,13 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT untuk tabel `anggota`
 --
 ALTER TABLE `anggota`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
@@ -542,12 +391,6 @@ ALTER TABLE `failed_jobs`
 --
 ALTER TABLE `kategori`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT untuk tabel `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `peminjaman`
@@ -568,12 +411,6 @@ ALTER TABLE `penulis`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `permissions`
---
-ALTER TABLE `permissions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT untuk tabel `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
@@ -583,54 +420,19 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT untuk tabel `petugas`
 --
 ALTER TABLE `petugas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- AUTO_INCREMENT untuk tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `user_role`
---
-ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `model_has_permissions`
---
-ALTER TABLE `model_has_permissions`
-  ADD CONSTRAINT `model_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `model_has_roles`
---
-ALTER TABLE `model_has_roles`
-  ADD CONSTRAINT `model_has_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `role_has_permissions`
---
-ALTER TABLE `role_has_permissions`
-  ADD CONSTRAINT `role_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `role_has_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
